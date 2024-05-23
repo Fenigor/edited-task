3300bbc6ad940e939fc38238739290b08190c467
“milen.stanchev” <milen.stanchev@emea.adecco.net>
2024-05-23 18:44:13 +0300

    feat:integrating logic inside view

--- crawler/views.py
+++ crawler/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
-
 from django.http import JsonResponse
 from django.db import connections
 from django.db.utils import OperationalError
 from datetime import datetime
+from .crawler import crawl_page
+from .models import WebResources
+from django.views.decorators.csrf import csrf_exempt
+from django.core import serializers
 
 def health_check(request):
     # Check database connection
@@ -17,22 +19,27 @@ def health_check(request):
     
     return JsonResponse({'status': 'ok', 'time': datetime.now().isoformat()})
 
-
+@csrf_exempt
 def screenshots(request):
     if request.method == 'POST':
-        start_crawler()
-        return
+        url = request.POST.get('start_url')
+        # print(request.body)
+        if not url:
+            return JsonResponse({'status': 'fail', 'message': 'start_url is required'}, status=400)
+        depth = request.POST.get('number_of_links_to_follow', 1)
+        return start_crawler(url, depth)
     if request.method == 'GET':
-        get_resources()
-        return
-    return JsonResponse({'status': 'ok', 'message': 'Screenshots taken'})
+        id = request.GET.get('id')
+        return get_resources(id)
 
 
-def start_crawler():
-    # This is a placeholder for the crawler start view
-    return JsonResponse({'status': 'ok', 'message': 'Crawler started'})
+def start_crawler(url, depth):
+    id, crawled_data = crawl_page(url, depth)
+    for url, pic in crawled_data:
+        WebResources.objects.create(link=url, image_reference=pic)
+    return JsonResponse({'status': 'ok', 'message': f'Crawler started for {url} with id {id}'})
 
 
-def get_resources():
-    # This is a placeholder for the view that returns the resources
-    return JsonResponse({'status': 'ok', 'message': 'Resources returned'})
+def get_resources(id):
+    resources = WebResources.objects.get(image_reference=f'{id}/{id}.png')
+    return JsonResponse({'status': 'ok', 'message': 'Resources returned', 'resource': serializers.serialize('json', [resources])})
